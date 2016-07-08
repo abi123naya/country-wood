badb3af63789cd26ef41d5f53082553a92cd1e9f
abi123naya <connect2abinaya@gmail.com>
2016-07-08 11:29:56 +0530

    Update and rename README.md to README.java
    finish

---
+++ README.java
@@ -0,0 +1,13 @@
+# country-wood
+import.java.util.*;
+class Check
+{ public static void main(String[] args)
+  { int x,i=0,sum=0;
+   System.out.println("Enter limit");
+   while(i<=x)
+   { sum=sum+i;
+   }
+    System.out.println("Sum of "+x+" numbers is :"+sum);
+  }
+
+}
